1aa87dedebc8c9d22e8221ee2daebe99947305bf
falsystack <falsystack@gmail.com>
2023-03-06 22:35:34 +0900

    feat: implements Header component

---
+++ src/components/molecules/Header/index.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <header>
+            <ul>
+                <li>Blog</li>
+                <li>Resume</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header;
