0accf847c604df10a66ffacd5f856ffea364d26d
falsystack <falsystack@gmail.com>
2023-03-06 22:50:54 +0900

    feat: add style to header components

--- src/components/molecules/Header/index.tsx
+++ src/components/molecules/Header/index.tsx
@@ -1,12 +1,36 @@
+import styled from "styled-components";
+
 const Header = () => {
     return (
-        <header>
-            <ul>
-                <li>Blog</li>
-                <li>Resume</li>
-            </ul>
-        </header>
+        <Container>
+            <List>
+                <ListItem>Blog</ListItem>
+                <ListItem>Resume</ListItem>
+            </List>
+        </Container>
     )
 }
 
 export default Header;
+
+const Container = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  background-color: antiquewhite;
+`
+
+const List = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  
+  padding-inline: 20px;
+  width: 150px;
+  font-size: 16px;
+  font-weight: 700;
+`
+
+const ListItem = styled.li`
+`
