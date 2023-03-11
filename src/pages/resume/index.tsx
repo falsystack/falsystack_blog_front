d39b4122fbf58f98964f863fcb64495597c0b62d
falsystack <falsystack@gmail.com>
2023-03-11 23:49:20 +0900

    feat: add resume page

---
+++ src/pages/resume/index.tsx
@@ -0,0 +1,13 @@
+import {Career} from "@/components/molecules/Career";
+
+const Resume = () => {
+    return (
+        <>
+            <Career />
+            <Career />
+            <Career />
+        </>
+    )
+}
+
+export default Resume;
